435b343ffae648953af51c853ff279dd637c18e5
Chicha Ilyes <chicha.ilyes.business@gmail.com>
2023-06-09 18:16:01 +0100

    make 404 look good

--- my-app/src/Pages/Error.js
+++ my-app/src/Pages/Error.js
@@ -1,13 +1,36 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-
+import '../css/error.css';
 function Error() {
   return (
     <div>
       <Helmet>
+        <meta charset="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link
+          href="https://fonts.googleapis.com/css?family=Fredoka+One"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Raleway:400,700"
+          rel="stylesheet"
+        />
         <title>Dinox - Error</title>
       </Helmet>
-      <h1>Sorry this page is not available</h1>
+      <div id="notfound">
+        <div className="notfound">
+          <div className="notfound-404">
+            <h1>404</h1>
+          </div>
+          <h2>Oops, The Page you are looking for can't be found!</h2>
+ 
+          <a href="/" className="mt-3">
+            <span className="arrow"></span>Return To Homepage
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
